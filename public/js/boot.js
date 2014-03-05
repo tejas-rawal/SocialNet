34bd0fa6bd5cf4d03cee2089340500dc7b3d56ad
Tejas Rawal <trawal90@gmail.com>
2014-03-05 14:39:32 -0600

    Add Account model with all features for user.

---
+++ public/js/boot.js
@@ -0,0 +1,14 @@
+require.config({
+  paths: {
+    jQuery: '/js/lib/jquery',
+    Underscore: '/js/lib/underscore',
+    Backbone: '/js/lib/backbone',
+    text: '/js/lib/text',
+    templates: '.../templates'
+  },
+
+  shim: {
+    'Backbone': ['Underscore', 'jQuery'],
+    'SocialNet': ['Backbone']
+  }
+});
