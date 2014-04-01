d27f2324a072ae8cf6024fc56d72b55d15f28856
Tejas Rawal <trawal90@gmail.com>
2014-04-01 15:17:10 -0500

    Updtae socialnet.js to include check to confirm if user is logged in.

--- public/js/router.js
+++ public/js/router.js
@@ -1,25 +1,39 @@
-define(['views/index', 'views/register', 'views/login', 'views/forgotpassword'])
-  function(IndexView, RegisterView, LoginView, ForgotPasswordView) {
-    var SocialRouter = Backbone.Router.extend({
-      currentview: null,
+define(['views/index', 'views/register', 'views/login', 'views/forgotpassword'],
+        function(IndexView, RegisterView, LoginView, ForgotPasswordView) {
+  var SocialRouter = Backbone.Router.extend({
+    currentview: null,
 
-      routes:{
-        'index': 'index',
-        'register': 'register',
-        'login': 'login',
-        'forgotpassword': 'forgotpassword'
-      },
+    routes:{
+      'index': 'index',
+      'register': 'register',
+      'login': 'login',
+      'forgotpassword': 'forgotpassword'
+    },
 
-      changeView: function(view) {
-        if(this.curentView != null) {
-          this.currentView.undelegateEvents();
-        }
-        this.currentView = view;
-        this.currentView.render();
-      },
-
-      index: function() {
-        this.changeView(new IndexView());
+    changeView: function(view) {
+      if(this.curentView != null) {
+        this.currentView.undelegateEvents();
       }
-    })
-  }
+      this.currentView = view;
+      this.currentView.render();
+    },
+
+    index: function() {
+      this.changeView(new IndexView());
+    },
+
+    login: function() {
+      this.changeView(new LoginView());
+    },
+
+    register: function() {
+      this.changeView(new RegisterView());
+    },
+
+    forgotpassword: function() {
+      this.changeView(new ForgotPasswordView());
+    }
+  });
+
+  return new SocialRouter();
+});
