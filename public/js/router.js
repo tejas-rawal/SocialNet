a4f46d3e3284915a79218871d6ca30bdc2a6691b
Tejas Rawal <trawal90@gmail.com>
2014-04-01 14:47:29 -0500

    Add router.js file to handle routing.

--- public/js/router.js
+++ public/js/router.js
@@ -1,1 +1,25 @@
-define
+define(['views/index', 'views/register', 'views/login', 'views/forgotpassword'])
+  function(IndexView, RegisterView, LoginView, ForgotPasswordView) {
+    var SocialRouter = Backbone.Router.extend({
+      currentview: null,
+
+      routes:{
+        'index': 'index',
+        'register': 'register',
+        'login': 'login',
+        'forgotpassword': 'forgotpassword'
+      },
+
+      changeView: function(view) {
+        if(this.curentView != null) {
+          this.currentView.undelegateEvents();
+        }
+        this.currentView = view;
+        this.currentView.render();
+      },
+
+      index: function() {
+        this.changeView(new IndexView());
+      }
+    })
+  }
