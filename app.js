c93ffaf5629c901aa0ef74f3d024f40e8644fd84
Tejas Rawal <trawal90@gmail.com>
2014-04-07 19:16:42 -0500

    Add server-side component for handling login event.

--- app.js
+++ app.js
@@ -31,6 +31,26 @@ app.post('/register', function(req, res) {
 
   Account.register(email, password, firstName, lastName);
   res.send(200);
-})
+});
+
+app.post('/login', function(req, res) {
+  console.log('login request');
+  var email = req.param('email', null);
+  var password = req.param('password', null);
+
+  if( email == null || email.length < 1 || password == null || password.length < 1 ) {
+    res.send(400);
+    return;
+  }
+
+  Account.login(email, password, function(success) {
+    if( !success ) {
+      res.send(401);
+      return;
+    }
+    console.log('login was successful');
+    res.send(200);
+  });
+});
 
 app.listen(8080);
